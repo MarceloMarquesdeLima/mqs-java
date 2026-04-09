247404ade27ed657d7ed4152a926c5cf9111205f
Marcelo Marques de Lima <111276703+MarceloMarquesdeLima@users.noreply.github.com>
2026-04-09 11:32:01 -0300

    Criação Cliente repository e Controller com ajuste no pom.xml

--- src/main/java/com/mqsdocker/mqs/controller/ClienteController.java
+++ src/main/java/com/mqsdocker/mqs/controller/ClienteController.java
@@ -1,5 +1,13 @@
 package com.mqsdocker.mqs.controller;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.mqsdocker.mqs.entity.Cliente;
+import com.mqsdocker.mqs.repository.ClienteRepository;
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
@@ -20,4 +28,5 @@ public class ClienteController {
         return repository.save(cliente);
     }
 }
+
 
--- src/main/java/com/mqsdocker/mqs/repository/ClienteRepository.java
+++ src/main/java/com/mqsdocker/mqs/repository/ClienteRepository.java
@@ -1,3 +1,7 @@
 package com.mqsdocker.mqs.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mqsdocker.mqs.entity.Cliente;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {}
